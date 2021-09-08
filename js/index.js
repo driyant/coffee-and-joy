94fe8b0f55288dcf3c83107545040dabf619f69d
unknown <driyant.mail@gmail.com>
2021-09-08 23:58:44 +0700

    Add event listener for auto closing navigation

--- js/index.js
+++ js/index.js
@@ -4,8 +4,18 @@ const hamburger = document.querySelector(".hamburger");
 
 // Callback function
 const toggleMenuOpen = () => {
-  navlinks.classList.toggle("open");
-  hamburger.classList.toggle("open");
+  navlinks.classList.add("open");
+  hamburger.classList.add("open");
 }
-
 hamburger.addEventListener("click", toggleMenuOpen);
+
+const removeOpen = () => {
+  setTimeout(() => {
+    navlinks.classList.remove("open");
+    hamburger.classList.remove("open");
+  }, 500);
+}
+const navLists = document.querySelectorAll(".nav__links li");
+navLists.forEach(navList => {
+  navList.addEventListener("click", removeOpen);
+});
